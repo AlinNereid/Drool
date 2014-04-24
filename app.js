3cfac1fc65969e8e8cdc12789908f33bdbba6300
AlinNereid <alin_alin_10@yahoo.com>
2014-04-24 17:41:12 +0300

    nodejs render html page and add module mongodb and request

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var express = require('express');
 var routes = require('./routes');
 var http = require('http');
 var path = require('path');
-
+var analy = require('./routes/analysis');
 var app = express();
 
 // all environments
@@ -27,6 +27,8 @@ if ('development' == app.get('env')) {
 }
 
 app.get('/', routes.index);
+app.get('/convertor', routes.index);
+app.get('/analysis', analy.analysis);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
