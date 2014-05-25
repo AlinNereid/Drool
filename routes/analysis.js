0c27dc4002395d4e45aaa537ed658d78674056a9
AlinNereid <alin_alin_10@yahoo.com>
2014-05-25 12:42:06 +0300

    make analysis

--- routes/analysis.js
+++ routes/analysis.js
@@ -1,10 +1,15 @@
 /**
  * Created by alin on 4/24/14.
  */
-
-
+var dbAPITicker = require('../models/dbAPITicker');
+var dbDigitalCoins = require('../models/dbDigitalCoins');
 var get = function (req, res) {
-    res.render('analysis', { title: 'Drool' });
+        dbDigitalCoins.getAllDigitalCoins(function(coins){
+            dbAPITicker.getAllApisWithDigSname(coins[0].sname,function(apis){
+                res.render('analysis', { title: 'Drool',apis:apis,digCoins:coins });
+            })
+        })
+
 };
 
 exports.get=get;
